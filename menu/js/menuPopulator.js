5e94692d9c39a3a855ae46489f755df3ab25b37d
JohnnysBBQRI <johnnysbbqri13@gmail.com>
2018-12-20 23:00:17 -0500

    change logging

--- menu/js/menuPopulator.js
+++ menu/js/menuPopulator.js
@@ -1,13 +1,11 @@
 function populateMenuDataFromFile(menuType) {
     jQuery.get('../menu/' + menuType + 'Column1' + '.txt', function(data) {
 		var column1GeneratedHtml = generateHtmlFromFileData(data)
-		console.log(menuType + 'Column1' + " -- column1data: " + column1GeneratedHtml)
         $(menuType + 'Column1').html(column1GeneratedHtml);
     });
 
     jQuery.get('../menu/' + menuType + 'Column2' + '.txt', function(data) {
         var column2GeneratedHtml = generateHtmlFromFileData(data)
-		console.log(menuType + 'Column2' + " -- column2data: " + column2GeneratedHtml)
         $(menuType + 'Column2').html(column2GeneratedHtml);
     });
 }
@@ -23,16 +21,11 @@ function getHtmlTemplate() {
 
 function generateHtmlFromFileData(fileData) {
     var fileLines = fileData.split('\n');
-	
-	console.log("fileLines: " + fileLines);
 
     var generatedHtml = '';
     for (index in fileLines) {
-		console.log("line: " + fileLines[index]);
         var lineData = fileLines[index].split(':');
-		console.log("lineData[0]: " + lineData[0]);
-		console.log("lineData[1]: " + lineData[1]);
-		console.log("lineData[2]: " + lineData[2]);
+		console.log(getHtmlTemplate())
         generatedHtml = generatedHtml.concat(getHtmlTemplate()
             .replace('{name}', lineData[0])
             .replace('{price}', lineData[1])
