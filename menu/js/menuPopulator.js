60481c55d217b5edd3b98a1bb0e693972ef42d4d
JohnnysBBQRI <johnnysbbqri13@gmail.com>
2018-12-20 21:43:15 -0500

    Implement dynamic menu (WIP)

---
+++ menu/js/menuPopulator.js
@@ -0,0 +1,42 @@
+function populateMenuDataFromFile(menuType) {
+    jQuery.get('../menu/' + menuType + 'Column1' + '.txt', function(data) {
+	    console.log("column1" + menuType + " " + data);
+		var column1GeneratedHtml = generateHtmlFromFileData(data)
+        $(menuType + 'Column1').html(column1GeneratedHtml);
+    });
+
+    jQuery.get('../menu/' + menuType + 'Column2' + '.txt', function(data) {
+		console.log("column2" + menuType + " " + data);
+        var column2GeneratedHtml = generateHtmlFromFileData(column2Data)
+        $(menuType + 'Column2').html(column2GeneratedHtml);
+    });
+}
+
+function getHtmlTemplate() {
+    jQuery.get('genericMenuTemplate.html', function(data) {
+        return data.replace('\n', '');
+    });
+}
+
+function generateHtmlFromFileData(fileData) {
+    var fileLines = fileData.split('\n');
+
+    var generatedHtml = '';
+    for (line in fileLines) {
+        var lineData = line.split(':');
+        generatedHtml += getHtmlTemplate()
+            .replace('{name}', lineData[0])
+            .replace('{price}', lineData[1])
+            .replace('{description}', lineData[2]);
+    }
+
+    return generatedHtml;
+}
+
+$(document).ready(function() {
+	console.log("loading all items...");
+    populateMenuDataFromFile('breakfast');
+    populateMenuDataFromFile('lunch');
+    populateMenuDataFromFile('bbq');
+    populateMenuDataFromFile('beverages');
+});
