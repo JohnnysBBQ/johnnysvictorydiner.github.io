010a086eaf5c797c539003b4a357045b90e3718b
JohnnysBBQRI <johnnysbbqri13@gmail.com>
2019-02-02 13:07:50 -0500

    log the lines

--- catering/js/cateringPopulator.js
+++ catering/js/cateringPopulator.js
@@ -32,7 +32,7 @@ function getEventProperties(eventId) {
     });
 	
 	fileLines = fileData.split('\n');
-	console.log(fileLines)
+	console.log(fileData)
 	return new EventProperties(fileLines[0].split('=')[1], fileLines[1].split('=')[1], fileLines[2].split('=')[1], fileLines[3].split('=')[1], fileLines[4].split('=')[1]);
 }
 
