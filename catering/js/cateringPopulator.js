2a583270b003fa956aa52148a3c57f9e7e17e8f3
JohnnysBBQRI <johnnysbbqri13@gmail.com>
2019-02-02 13:17:09 -0500

    populate event link

--- catering/js/cateringPopulator.js
+++ catering/js/cateringPopulator.js
@@ -1,7 +1,8 @@
 class EventProperties {
-	constructor(imageLink, videoLink, date, title, description) {
+	constructor(imageLink, videoLink, eventLink, date, title, description) {
 		this.imageLink = imageLink;
 		this.videoLink = videoLink;
+		this.eventLink = eventLink;
 		this.date = date;
 		this.title = title;
 		this.description = description;
@@ -37,7 +38,7 @@ function getEventProperties(eventId) {
     });
 	
 	fileLines = fileData.split('\n');
-	return new EventProperties(fileLines[0].split('=')[1], fileLines[1].split('=')[1], fileLines[2].split('=')[1], fileLines[3].split('=')[1], fileLines[4].split('=')[1]);
+	return new EventProperties(fileLines[0].split('=')[1], fileLines[1].split('=')[1], fileLines[2].split('=')[1], fileLines[3].split('=')[1], fileLines[4].split('=')[1], fileLines[5].split('=')[1]);
 }
 
 function getHtmlTemplate(templateUrl) {
@@ -59,6 +60,7 @@ function getHtmlTemplate(templateUrl) {
 function generateHtmlFromEventProperties(eventProperties, htmlTemplate) {
 	return htmlTemplate.replace('{imageLink}', eventProperties.imageLink)
 		.replace('{videoLink}', eventProperties.videoLink)
+		.replace('{eventLink}', eventProperties.eventLink === null ? '' : eventProperties.eventLink)
 		.replace('{date}', eventProperties.date === null ? '' : eventProperties.date)
 		.replace('{title}', eventProperties.title === null ? '' : eventProperties.title)
 		.replace('{description}', eventProperties.description === null ? '' : eventProperties.description);
