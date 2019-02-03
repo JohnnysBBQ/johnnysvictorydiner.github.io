c8b340de1e1a5bf32664f456ab7abed6c21ff144
JohnnysBBQRI <johnnysbbqri13@gmail.com>
2019-02-03 15:51:01 -0500

    rework script

--- catering/js/cateringPopulator.js
+++ catering/js/cateringPopulator.js
@@ -1,17 +1,22 @@
-function EventProperties(imageLink, videoLink, eventLink, date, title, description) {
-	this.imageLink = imageLink;
-	this.videoLink = videoLink;
-	this.eventLink = eventLink;
-	this.date = date;
-	this.title = title;
-	this.description = description;
-}
-
 function populateEventDataFromFile(eventId) {
-	var eventProperties = getEventProperties(eventId);
-	var templateUrl = eventProperties.imageLink ? "../catering/js/cateringImageTemplate.txt" : "../catering/js/cateringVideoTemplate.txt";
+    var eventPropertiesFileLines = getEventProperties(eventId);
+	
+	var imageLink = eventPropertiesFileLines[0].split('~')[1];
+	var videoLink = eventPropertiesFileLines[1].split('~')[1];
+	var eventLink = eventPropertiesFileLines[2].split('~')[1];
+	var date = eventPropertiesFileLines[3].split('~')[1];
+	var title = eventPropertiesFileLines[4].split('~')[1];
+	var description = eventPropertiesFileLines[5].split('~')[1];
+	
+    var templateUrl = imageLink ? "../catering/js/cateringImageTemplate.txt" : "../catering/js/cateringVideoTemplate.txt";
     var htmlTemplate = getHtmlTemplate(templateUrl);
-    var generatedHtml = generateHtmlFromEventProperties(eventProperties, htmlTemplate);
+	
+    var generatedHtml =  htmlTemplate.replace('{imageLink}', imageLink)
+        .replace('{videoLink}', videoLink)
+        .replace('{eventLink}', eventLink ? eventLink : '')
+        .replace('{date}', date ? date : '')
+        .replace('{title}', title ? title : '')
+        .replace('{description}', description ?  description : '');
 	
 	$('#' + eventId).html(generatedHtml);
 }
@@ -30,8 +35,7 @@ function getEventProperties(eventId) {
         }
     });
 	
-	fileLines = fileData.split('\n');
-	return new EventProperties(fileLines[0].split('~')[1], fileLines[1].split('~')[1], fileLines[2].split('~')[1], fileLines[3].split('~')[1], fileLines[4].split('~')[1], fileLines[5].split('~')[1]);
+    fileLines = fileData.split('\n');
 }
 
 function getHtmlTemplate(templateUrl) {
@@ -48,15 +52,6 @@ function getHtmlTemplate(templateUrl) {
         }
     });
     return htmlTemplate;
-}
-
-function generateHtmlFromEventProperties(eventProperties, htmlTemplate) {
-	return htmlTemplate.replace('{imageLink}', eventProperties.imageLink)
-		.replace('{videoLink}', eventProperties.videoLink)
-		.replace('{eventLink}', eventProperties.eventLink ? eventProperties.eventLink : '')
-		.replace('{date}', eventProperties.date ? eventProperties.date : '')
-		.replace('{title}', eventProperties.title ? eventProperties.title : '')
-		.replace('{description}', eventProperties.description ?  eventProperties.description : '');
 }
 
 $(document).ready(function() {
