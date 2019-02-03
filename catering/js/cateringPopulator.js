5417c72a1b45b481d06c4bd7a45c02a17f963946
JohnnysBBQRI <johnnysbbqri13@gmail.com>
2019-02-03 15:21:21 -0500

    testing

--- catering/js/cateringPopulator.js
+++ catering/js/cateringPopulator.js
@@ -13,7 +13,6 @@ function populateEventDataFromFile(eventId) {
 	var eventProperties = getEventProperties(eventId);
 	var templateUrl = eventProperties.imageLink ? "../catering/js/cateringImageTemplate.txt" : "../catering/js/cateringVideoTemplate.txt";
     var htmlTemplate = getHtmlTemplate(templateUrl);
-    var generatedHtml = generateHtmlFromEventProperties(eventProperties, htmlTemplate);
     
 	console.log('-' + eventProperties);
 	console.log('--' + templateUrl);
