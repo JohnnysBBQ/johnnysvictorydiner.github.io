06fee2b36abe42ff4f7043e27bbbbe4f04846e13
JohnnysBBQRI <johnnysbbqri13@gmail.com>
2019-02-03 16:02:41 -0500

    adding things slowly

--- catering/js/cateringPopulator.js
+++ catering/js/cateringPopulator.js
@@ -1,3 +1,12 @@
+function EventProperties(imageLink, videoLink, eventLink, date, title, description) {
+	this.imageLink = imageLink;
+	this.videoLink = videoLink;
+	this.eventLink = eventLink;
+	this.date = date;
+	this.title = title;
+	this.description = description;
+}
+
 function populateEventDataFromFile(eventId) {
 	console.log('called function populate');
 }
