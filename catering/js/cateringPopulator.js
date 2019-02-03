583cac8521ef5f66f7ba2910eabda4eea7f8b068
JohnnysBBQRI <johnnysbbqri13@gmail.com>
2019-02-03 16:49:47 -0500

    Update cateringPopulator.js

--- catering/js/cateringPopulator.js
+++ catering/js/cateringPopulator.js
@@ -8,7 +8,7 @@ function EventProperties(imageLink, videoLink, eventLink, date, title, descripti
 }
 
 function getEventProperties(eventId) {
-    fileData = '';
+    var fileData = '';
     $.ajax({
         url: '../catering/' + eventId + '.txt',
         type: "get",
@@ -21,24 +21,8 @@ function getEventProperties(eventId) {
         }
     });
 	
-    fileLines = fileData.split('\n');
+    var fileLines = fileData.split('\n');
     return new EventProperties(fileLines[0].split('~')[1], fileLines[1].split('~')[1], fileLines[2].split('~')[1], fileLines[3].split('~')[1], fileLines[4].split('~')[1], fileLines[5].split('~')[1]);
-}
-
-function getHtmlTemplate(templateUrl) {
-    var htmlTemplate = '';
-    $.ajax({
-        url: templateUrl,
-        type: "get",
-        async: false,
-        success: function(data) {
-            htmlTemplate = data.replace('\n', '');
-        },
-        error: function() {
-            console.log('Could not load event data');
-        }
-    });
-    return htmlTemplate;
 }
 
 function populateEventDataFromFile(eventId) {
